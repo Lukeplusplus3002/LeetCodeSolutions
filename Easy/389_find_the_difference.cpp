ed4d68a98d56c1d0995c41541e1f796b9dbad09d
Lucas Martínez <luquitasamv@gmail.com>
2025-07-05 03:35:53 -0300

    "Find The Difference" problem added. Solution is complete

---
+++ Easy/389_find_the_difference.cpp
@@ -0,0 +1,17 @@
+//problem link: https://leetcode.com/problems/find-the-difference/
+#include<string>
+using std::string;
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        const size_t sizeOfS{s.size()};
+        unsigned int sumOfS{}, sumOfT{};//these two variables are for adding each character of both strings with one another
+        for(size_t i=0; i<sizeOfS; ++i)
+        {
+            sumOfS+=s[i];
+            sumOfT+=t[i];
+        }
+        sumOfT+=t[sizeOfS];
+        return sumOfT-sumOfS;//just return the difference between both sums, which is the ASCII value of the added character
+    }
+};
