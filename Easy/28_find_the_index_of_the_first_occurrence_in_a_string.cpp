6ddd51a680e4c600acebc822cfa6c6d3c61d2529
Lucas Martínez <luquitasamv@gmail.com>
2025-07-06 00:04:44 -0300

    removed useless else and changed parameters to constant references  in this file

--- Easy/28_find_the_index_of_the_first_occurrence_in_a_string.cpp
+++ Easy/28_find_the_index_of_the_first_occurrence_in_a_string.cpp
@@ -3,25 +3,23 @@
 using std::string, std::string_view;
 class Solution {
 public:
-    int strStr(const string haystack, const string needle) {//no need to modify the parameters
+    int strStr(const string &haystack, const string &needle) {//no need to modify the parameters
         const size_t sizeOfNeedle{needle.size()};
         if(sizeOfNeedle>haystack.size())
         {
             return -1;
-        }else
-        {
-            size_t i{};//index for each character of haystack
-            const size_t lastIndex{haystack.size()-sizeOfNeedle};//greatest valid index i can be
-            for(i; i<=lastIndex; ++i)
-            {
-                const string_view haystackWord{&haystack[i], sizeOfNeedle};//use of string_view (a string reference) to avoid creating copies
-                if(haystackWord==needle)
-                {
-                    return i;
-                }
-            }
-            return -1;
         }
+        size_t i{};//index for each character of haystack
+        const size_t lastIndex{haystack.size()-sizeOfNeedle};//greatest valid index i can be
+        for(i; i<=lastIndex; ++i)
+        {
+            const string_view haystackWord{&haystack[i], sizeOfNeedle};//use of string_view (a string reference) to avoid creating copies
+            if(haystackWord==needle)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 };
 /*
