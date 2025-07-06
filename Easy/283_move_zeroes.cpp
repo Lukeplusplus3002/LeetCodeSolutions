dac3bccb230229ff06e51f893673d283f3fb33e4
Lucas Martínez <luquitasamv@gmail.com>
2025-07-06 18:08:14 -0300

    added new solution

--- Easy/283_move_zeroes.cpp
+++ Easy/283_move_zeroes.cpp
@@ -1,6 +1,7 @@
 //problem link: https://leetcode.com/problems/move-zeroes/
 #include<vector>
 using std::vector, std::swap; 
+//this solution performed better on LeetCode, but it's harder to read
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -23,6 +24,25 @@ public:
             }else
             {
                 swap(currentIPlusOne, nums[i-zeroes+1]);//swap the element after nums[i] with the first zero of the array
+            }
+        }
+    }
+};
+//this solution performed worse, but it's much easier to follow
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        const size_t sizeOfNums{nums.size()};
+        size_t zeroes{};//counts how many zeroes have been encountered
+        for(size_t i=0; i<sizeOfNums; ++i)
+        {
+            int &currentI{nums[i]};
+            if(currentI==0)
+            {
+                ++zeroes;
+            }else if(zeroes!=0)//don't swap if there are no preceding zeroes
+            {
+                swap(currentI, nums[i-zeroes]);
             }
         }
     }
