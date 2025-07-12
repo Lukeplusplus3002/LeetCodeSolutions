420a5855904495e983adc39597abef22dfe0df8b
Lucas Martínez <luquitasamv@gmail.com>
2025-07-12 12:32:45 -0300

    improved comment formatting

--- Easy/1502_can_make_arithmetic_progression_from_sequence.cpp
+++ Easy/1502_can_make_arithmetic_progression_from_sequence.cpp
@@ -2,7 +2,8 @@
 #include<vector>
 #include <algorithm>
 using std::vector, std::sort;
-//the solution below has O(n*log(n)) time complexity due to sorting. Not the best solution performance-wise, but very readable
+//the solution below has O(n*log(n)) time complexity due to sorting. Not the best solution 
+//performance-wise, but very readable
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
@@ -25,11 +26,14 @@ class Solution {
 public:
 /*
     FORMULA USED
-    a_i=a_0+diff*i, where a_i is the ith term of the progression, diff is the common difference, a_0 is the first term, and i is the index of a_i
+    a_i=a_0+diff*i, where a_i is the ith term of the progression, diff is the common difference, 
+    a_0 is the first term, and i is the index of a_i
 */
     bool canMakeArithmeticProgression(vector<int>& arr) {
         const size_t lastIndex{arr.size()-1};
-        int &min{arr[0]}, &max{arr[lastIndex]};//the lowest term is meant to be in index 0, while the greatest term is meant to be in the last index
+        int &min{arr[0]}, &max{arr[lastIndex]};//the lowest term is meant to be in index 0, while 
+        //the greatest term is meant to be in the last index
+        
         for(int &current : arr)
         {
             if(current<min)
@@ -41,7 +45,11 @@ public:
             }
         }
         //by this point, min actually holds the lowest value, while max actually holds the greatest value
-        if((max-min) % lastIndex !=0)//if diff isn't a whole number, an arithmetic progression is impossible (see diff declaration below)
+        if((max-min) % lastIndex !=0)
+        /*
+        if diff isn't a whole number, an arithmetic progression is impossible 
+        see diff declaration below if the comment above is confusing
+        */
         {
             return false;
         }
@@ -51,20 +59,38 @@ public:
             return true;
         }
         size_t i{1};
-        while(i<lastIndex)//I didn't use a for loop because I just want to increment i when it's pointing to a number that's already in its corresponding position. In other words, i keeps pointing to the same position in the array until the corresponding number ends up in there
+        while(i<lastIndex)
+        /*
+            I decided not to use a for loop because I just want to 
+            increment i when it becomes the index of a number that's 
+            in its correct position
+        */
         {
             int &current{arr[i]};
             /*
-            All operations in this loop are just derived from the formula I wrote at the beginning of the program (a_i=a_0+diff*i). Keep in mind: the reference "current" represents a_i and the reference "min" represents a_0
+            All operations in this loop are just derived from the formula 
+            I wrote at the beginning of the program (a_i=a_0+diff*i). 
+            Keep in mind: the reference "current" represents a_i and the reference 
+            "min" represents a_0
             */
-            if((current-min) % diff != 0)//this is true when you try to find the index of a number that can't mathematically fit in the progression
+            if((current-min) % diff != 0)//this is true when you try to find the index of 
+            //a number that can't mathematically fit in the progression
             {
                 return false;
             }
-            const size_t actualIndex{static_cast<size_t>((current-min)/diff)};//actualIndex is the position in which "current" must sit within the progression, while "i" is where "current" is currrently sitting
+            const size_t actualIndex{static_cast<size_t>((current-min)/diff)};
+            //actualIndex is the position in which "current" must sit within the progression, 
+            //while "i" is where "current" is currrently sitting
             if(i != actualIndex)
             {
-                if((arr[actualIndex]-min)/diff != actualIndex)//by this point, "current" needs to be in position "actualIndex", but if the number that's in actualIndex is already in its correct position, you can't swap. If you can't swap, it means you have two numbers that must sit in the same position, so the array is not an arithmetic progression
+                if((arr[actualIndex]-min)/diff != actualIndex)
+                /*
+                by this point, "current" needs to be in position "actualIndex", but if 
+                the number that's in actualIndex is already in its correct position, 
+                you can't swap. If you can't swap, it means you have two numbers that 
+                must sit in the same position, so the array is not an arithmetic 
+                progression
+                */
                 {
                     swap(current, arr[actualIndex]);
                 }else
